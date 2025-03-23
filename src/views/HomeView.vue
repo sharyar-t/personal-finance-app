5522b890b7b2570fea3a4588bc5372a2d8e7f676
sharyar-dev <onshariiit@gmail.com>
2025-03-23 11:29:15 +0500

    delete unused files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import {Button} from "@/components/ui/button";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Button>Click me</Button>
   </main>
 </template>
