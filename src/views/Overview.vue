aca1cb4f291daa2e13208a6270a1efe78f3947f6
sharyar-dev <onshariiit@gmail.com>
2025-03-26 19:58:20 +0500

    update Overview layout to improve spacing between cards

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -9,7 +9,7 @@ import TransactionsCard from '@/components/overview/TransactionsCard.vue'
     <div class="mb-8">
       <h1 class="text-2xl font-medium">Overview</h1>
     </div>
-    <div class="mb-8 grid gap-3 md:grid-cols-3">
+    <div class="mb-8 grid gap-3 md:grid-cols-3 md:gap-6">
       <OverviewCard active title="$4,836.00" subtitle="Current Balance" />
       <OverviewCard title="$3,814.25" subtitle="Income" />
       <OverviewCard title="$1,700.50" subtitle="Expenses" />
